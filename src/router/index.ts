5900206cad9e3fb27776723ad34b4b62038ce6a4
Katficz <gawlak89@gmail.com>
2023-06-14 21:47:37 +0200

    structure folders and components

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,17 +4,13 @@ import HomeView from "../views/HomeView.vue";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
+    name: "InternsList",
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/",
+    name: "PreviewOneIntern",
+    component: HomeView,
   },
 ];
 
